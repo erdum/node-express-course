96a0db235258041a61298caca1b6269561e1c2c5
Syed M Erdum Adnan <erdumadnan@gmail.com>
2022-06-11 23:49:32 +0500

    first route added

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
 
+const mockUserData = [
+	{name:'Mark'},
+	{name:'Jill'}
+];
+
+app.get("/users", (req, res) => {
+	res.json({
+		success: true,
+		message: "success",
+		data: mockUserData,
+	});
+});
+
 app.listen(3000, () => {
 	console.log("server started on port 3000");
 });
