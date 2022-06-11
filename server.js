045456942d3138e1bd9a456b71cafc652dab99e0
Syed M Erdum Adnan <erdumadnan@gmail.com>
2022-06-12 00:00:55 +0500

    POST login route added

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 const mockUserData = [
 	{name:'Mark'},
 	{name:'Jill'}
 ];
+
+app.use(bodyParser.json());
 
 app.get("/users/:id", (req, res) => {
 	console.log(req.params.id);
@@ -13,6 +16,26 @@ app.get("/users/:id", (req, res) => {
 		message: "success",
 		data: mockUserData,
 	});
+});
+
+app.post("/login", (req, res) => {
+	const userName = req.body.username;
+	const userPass = req.body.userpass;
+	const mockName = "Erdum";
+	const mockPass = "1234";
+
+	if (userName === mockName && userPass === mockPass) {
+		res.json({
+			success: true,
+			message: "success",
+			data: mockUserData,
+		});
+	} else {
+		res.jsoN({
+			success: false,
+			message: "password and username do not match",
+		});
+	}
 });
 
 app.listen(3000, () => {
